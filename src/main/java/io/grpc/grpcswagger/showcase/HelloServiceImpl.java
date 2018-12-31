91e94787bebafb558fc6be231c031048622c8a7b
liuzhengyang <liuzhengyang@kuaishou.com>
2018-12-31 11:26:01 +0800

    增加测试方法，支持更多类型

--- src/main/java/io/grpc/grpcswagger/showcase/HelloServiceImpl.java
+++ src/main/java/io/grpc/grpcswagger/showcase/HelloServiceImpl.java
@@ -2,10 +2,15 @@ package io.grpc.grpcswagger.showcase;
 
 import static java.util.Collections.singletonMap;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 
 import io.grpc.grpcswagger.showcase.HelloProto.HelloRequest;
 import io.grpc.grpcswagger.showcase.HelloProto.HelloResponse;
@@ -27,6 +32,30 @@ public class HelloServiceImpl extends HelloServiceImplBase {
                 .putAllMap(map)
                 .build();
         responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getUser(HelloProto.GetUserRequest request, StreamObserver<HelloProto.GetUserResponse> responseObserver) {
+        long userId = request.getUserId();
+        List<HouseProto.House> houses = new ArrayList<>();
+        houses.add(HouseProto.House.newBuilder().setCity("北京").setId(1).build());
+        houses.add(HouseProto.House.newBuilder().setCity("海南").setId(2)
+                .setPrice(1.2f).build());
+        HelloProto.User user = HelloProto.User.newBuilder()
+                .setUserId(userId)
+                .setAge(0)
+                .setScore(1.1)
+                .setUserName("user test")
+                .putAllProperties(ImmutableMap.of("key", 1L))
+                .addAllAlias(Lists.newArrayList("zhangsan", "lisi"))
+                .addAllHouses(houses)
+                .build();
+        HelloProto.GetUserResponse userResponse = HelloProto.GetUserResponse.newBuilder()
+                .setStatus(HelloProto.GetUserResponse.Status.OK)
+                .setUser(user)
+                .build();
+        responseObserver.onNext(userResponse);
         responseObserver.onCompleted();
     }
 }
